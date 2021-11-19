d19298c28043ff3747fbaf5894409338c48ae85b
udayk <udayk@DESKTOP-UJFK23I>
2021-11-19 08:51:36 +0530

    Test Project commit to remote repo

---
+++ TestProjectGit/src/main/java/test/pack/ChromeBrowserLaunch.java
@@ -0,0 +1,21 @@
+package test.pack;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeBrowserLaunch {
+	// template // Ctrl+ space bar --> enter
+ public static void main(String[] args) throws Exception {
+	 // open chrome browser
+	 WebDriverManager.chromedriver().setup(); // invoke chrome driver	 
+	 WebDriver driver = new ChromeDriver(); // chrome browser
+	
+	 // open application --google
+	 driver.get("https://www.google.com/");
+	 //wait
+	 Thread.sleep(3000);
+	 //close browser
+	 driver.close();
+}
+}
